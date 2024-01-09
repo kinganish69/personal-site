1e8d66d2d28ed23d23d1c450f1de68e74eea4b35
King OP <97921035+kinganish69@users.noreply.github.com>
2024-01-10 00:01:37 +0545

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ window.onscroll = () => {
         }
     })
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navList = document.querySelector('.navlist');
+
+    menuIcon.addEventListener('click', function() {
+        navList.classList.toggle('open');
+    });
+});
